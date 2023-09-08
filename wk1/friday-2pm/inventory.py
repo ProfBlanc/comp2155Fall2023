e579884e09c808f2c56db26f072bfedd19b76474
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2023-09-08 15:35:06 -0400

    Update inventory.py

--- wk1/friday-2pm/inventory.py
+++ wk1/friday-2pm/inventory.py
@@ -18,12 +18,20 @@ try:
     print(f"Your budget is ${budget}")
     
     total_price = 0
+    number_of_items = 0
     
     while total_price < budget:
         name = input("Enter name of article: ")
         description = input(f"Enter description of {name}: ")
         price = float(input(f"The price of {name} is?: "))
+        if price + total_price > budget:
+            print("Cannot add item. Cannot exceed budget.")
+            print(f"You only have ${budget - total_price}. Remaining in budget. Please try again.")
+            continue
+        number_of_items += 1
         total_price += price
+        print(f"{name} added to shopping cart.")
+    print(f"You bought {number_of_items} for a total of ${total_price}")    
 except TypeError:
     print("Invalid budget value")
 except ValueError:
