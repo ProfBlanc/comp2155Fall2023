e57e425645340a39c68982a330d66defd36cd458
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2023-09-11 11:34:10 -0400

    Update inventory.py

--- wk2/monday-10am/inventory.py
+++ wk2/monday-10am/inventory.py
@@ -13,14 +13,31 @@ try:
     budget = int(answer)
     print(f"My budget is ${budget} dollars")
     amount_of_money_spent = 0
+    purchases = list()
     while amount_of_money_spent < budget:
         name = input("Enter name of product: ")
         description = input("Describe " + name + ": ")
         price = float(input(f"The price of {name} is: "))
+        if amount_of_money_spent + price > budget:
+            print(f"Cannot buy {name} because it is "
+                  f"${(amount_of_money_spent + price ) - budget}"
+                  f" over the budget. "
+                  "Please buy something that cost no more than "
+                  f"${budget - amount_of_money_spent} dollars")
+            continue  # break to stop the spending
         amount_of_money_spent += price
-    print(f"You have spent "
+        purchases.append({"name": name, "description": description,
+                          "price": price})
+    print(f"You have purchased {len(purchases)} items. You have spent "
           f"${amount_of_money_spent} dollars today")
 
+    file = open("receipt.txt", "w")
+    file.write("Store Receipt\n\n")
+    file.write("Name " + '\t' * 4 + "Description" + '\t' * 8 + "Price" + '\n')
+    for item in purchases:
+        file.write(item['name'] + "\t" * 4 + item['description']
+                   + "\t" * 8 + str(item['price']) + "\n")
+    file.write("Total" + '\t' * 12 + str(amount_of_money_spent))
 except TypeError:
     print("Invalid budget value", file=sys.stderr)
 except ValueError:
